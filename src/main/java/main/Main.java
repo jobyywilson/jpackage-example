1d995d14d775f0a68390bbf0bff0085f21dd10f9
DC\joby.w <jobyyywilson@gmail.com>
2021-04-05 07:21:44 +0530

    initial commit

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import javax.swing.*;
+
+public class Main{
+    public static void main(String args[]){
+        JFrame f=new JFrame();
+        JButton b=new JButton("click");
+        b.setBounds(130,100,100, 40);
+        f.add(b);
+        f.setSize(400,300);
+        f.setLayout(null);
+        f.setVisible(true);
+
+    }
+}
